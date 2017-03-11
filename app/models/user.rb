7538810c3a28b092e25d26387c984ec1854b56f4
Javier M. Fonseca <jmfonseca66@gmail.com>
2017-03-11 17:43:55 -0600

    Primer cambio

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+has_many :tweets
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+ mount_uploader :image,  ImageUploader
+
+end
